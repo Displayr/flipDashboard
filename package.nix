e68a737446db0361430fab9580dc62f6ff2f3913
Jordan Cran <jordancran@hey.com>
2025-05-05 14:07:27 +1000

    DPI-2964: use nix CI

--- package.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {}, displayrUtils }:
-
-pkgs.rPackages.buildRPackage {
-  name = "flipDashboard";
-  version = displayrUtils.extractRVersion (builtins.readFile ./DESCRIPTION); 
-  src = ./.;
-  description = "Tools that help when creating a Dashboard in Displayr, such as for filtering using comboxes.";
-}
